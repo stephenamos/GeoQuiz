3e8e78b7798d41c38c080f75da442bf0af8bedaf
stephenamos <amos.stephen@gmail.com>
2013-07-25 16:20:32 -0700

    Added UI widgets and components.

--- gen/com/bignerdranch/android/geoquiz/R.java
+++ gen/com/bignerdranch/android/geoquiz/R.java
@@ -24,7 +24,9 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080002;
+        public static final int false_button=0x7f080001;
+        public static final int true_button=0x7f080000;
     }
     public static final class layout {
         public static final int activity_quiz=0x7f030000;
@@ -33,9 +35,11 @@ public final class R {
         public static final int quiz=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
+        public static final int action_settings=0x7f050004;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int false_button=0x7f050003;
+        public static final int question_text=0x7f050001;
+        public static final int true_button=0x7f050002;
     }
     public static final class style {
         /** 
