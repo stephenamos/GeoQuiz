0a0d6ceae424f437216e2dd3ce95c8d7194a9cde
stephenamos <amos.stephen@gmail.com>
2013-07-26 10:07:24 -0700

    Created second Activity for cheat menu

---
+++ src/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -0,0 +1,18 @@
+package com.bignerdranch.android.geoquiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CheatActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_cheat);
+	}
+	
+	
+
+	
+}
