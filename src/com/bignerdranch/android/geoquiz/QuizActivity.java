afd9a0222d0431c7bbe6ca38f74883787080e932
stephenamos <amos.stephen@gmail.com>
2013-07-25 21:08:27 -0700

    Added logging to view Activity's state change call methods.  Added landscape view XML.

--- src/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ src/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.geoquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +11,8 @@ import android.widget.Toast;
 
 public class QuizActivity extends Activity {
 
+	private static final String TAG = "QuizActivity";
+	
 	private Button mTrueButton;
 	private Button mFalseButton;
 	private Button mNextButton;
@@ -30,6 +33,7 @@ public class QuizActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Log.d(TAG, "onCreate(Bundle) called.");
 		setContentView(R.layout.activity_quiz);
 
 		mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
@@ -108,6 +112,48 @@ public class QuizActivity extends Activity {
 		}
 
 		Toast.makeText(QuizActivity.this, messageResId, Toast.LENGTH_SHORT).show();
+	}
+
+	
+	/**
+	 * The default implementation of onSaveInstanceState(...) asks all of the activity's views(The UI widgets) to save their
+	 * state as data in the Bundle object. 
+	 * 
+	 * @param outState A Bundle is a structure that maps string keys to values of certain limited types.
+	 */
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		Log.d(TAG, "onDestroy() method called.");
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		Log.d(TAG, "onPause() method called.");
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Log.d(TAG, "onResume() method called.");
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		Log.d(TAG, "onStart() method called.");
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		Log.d(TAG, "onStop() method called.");
 	}
 
 }
